4191e5bf6eab021934930f6fd7e552d5982a4154
matt <thevetab@gmail.com>
2016-02-29 21:03:01 -0800

    outline files

---
+++ js/scripts.js
@@ -0,0 +1,73 @@
+var factorial = function(number) {
+  for (var index = number; index = number.length; index - 1) {
+  (factorial[number]);
+}
+
+}
+
+var factorial = function(number) {
+for (i = 5; i <= 5; i++)
+{
+factorial *= i;
+}
+
+
+
+
+// factorial loop example 1
+
+// var factorial = 1;
+// for (i = 1; i <= 5; i++)
+// {
+//   factorial *= i;
+// }
+// will do factorial 5, I'm sure you can figure out from there how to generalise it...
+
+
+
+// factorial loop example 2
+
+// function factorial (n){
+//   j = 1;
+//   for(i=1;i<=n;i++){
+//     j = j*i;
+//   }
+//   return j;
+// }
+
+
+
+// code from leapyear og ducument:
+
+
+// var leapYear = function(year) {
+//   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+//       return true;
+//     } else {
+//       return false;
+
+// }
+
+//   };
+
+//   $(document).ready(function() {
+//     $("form#leap-year").submit(function(event) {
+//         var year = parseInt($("input#year").val());
+//         var result = leapYear(year);
+
+//         $(".year").text(year);
+
+//         if (!result) {
+//           $(".not").text("not");
+//         } else {
+//           $(".not").text("");
+//         }
+
+//         $("#result").show();
+//         event.preventDefault();
+
+
+
+//   });
+
+// });
